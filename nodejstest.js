201f886e27182ecbe60a46f4c4de299e811855e1
Annie  Lin <https://github.com/linannie/linannie.github.io/commit/7c494e04c2cf7492290aecd4a064ccb7e3416ee6.patch>
2017-12-30 21:29:09 -0800

    first commit

---
+++ nodejstest.js
@@ -0,0 +1,11 @@
+/* Load HTTP library */
+
+var http = require("http");
+
+/* create HTTP server to handle responses */
+
+http.createServer(function(request, response){
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello World");
+	response.end();
+}).listen(8888);
